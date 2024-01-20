253fdbb1b7c57f1cf3b0700665b9ba9fa996eb0f
johnCando <john.doe@candogram.com>
2024-01-20 09:16:08 -0500

    updated PROD IP address in quasar.config.js

--- client/quasar.config.js
+++ client/quasar.config.js
@@ -62,7 +62,7 @@ module.exports = configure(function ( ctx ) {
 
       vueRouterMode: 'history', // available values: 'hash', 'history'
       env: {
-        SERVER_URL: ctx.dev ? 'http://localhost:3000' : 'http://54.242.195.87',
+        SERVER_URL: ctx.dev ? 'http://localhost:3000' : 'http://54.163.35.60',
         CREATOR_NAME: 'Henning Seip',
         CREATOR_EMAIL: 'henning@candogram.com',
         LINKEDIN: 'https://www.linkedin.com/in/henningseip/'
