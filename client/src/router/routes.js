cc662683dbb51d2ef8681f9961f2da173c7b572b
henning seip <henning.seip@candogram.com>
2025-11-16 19:21:20 -0500

    Student Survey client is complete

--- client/src/router/routes.js
+++ client/src/router/routes.js
@@ -2,15 +2,18 @@ const routes = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+    children: [
+      { path: '', component: () => import('src/pages/HomePage.vue'), name: 'Home' },
+      { path: 'survey', component: () => import('src/pages/SurveyPage.vue'), name: 'Survey'},
+      { path: 'results', component: () => import('src/pages/ResultsPage.vue'), name: 'Results' }
+    ]
   },
 
   // Always leave this as last one,
   // but you can also remove it
   {
     path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue'),
-  },
+    component: () => import('pages/ErrorNotFound.vue')
+  }
 ]
-
 export default routes
